fb660a4d1c1aafacfaca5d615f260fb416759d95
firatmelih <melih.firat@ogr.sakarya.edu.tr>
2021-01-16 21:31:24 +0300

    Design changes

--- script/script.js
+++ script/script.js
@@ -2,7 +2,7 @@ const upArrow = document.getElementById('up');
 const downArrow = document.getElementById('down');
 
 let down = false;
-const halfOfPage = document.documentElement.clientHeight / 4;
+const halfOfPage = document.documentElement.clientHeight / 5;
 
 downArrow.hidden = false;
 upArrow.hidden = true;
@@ -27,3 +27,16 @@ downArrow.addEventListener('click', () => {
 upArrow.addEventListener('click', () => {
   window.scrollTo(0, 0);
 });
+var pageWidth = document.documentElement.clientWidth;
+window.setInterval(function check() {
+  if (pageWidth != document.documentElement.clientWidth) {
+    pageWidth = document.documentElement.clientWidth;
+    if (pageWidth < 850) {
+      document.getElementById('container-1').classList.add('large');
+      document.getElementById('container-1').classList.remove('normal');
+    } else {
+      document.getElementById('container-1').classList.add('normal');
+      document.getElementById('container-1').classList.remove('large');
+    }
+  }
+}, 500);
