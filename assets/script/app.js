d8adb74719e6427c4329968cae401518b3791500
firatmelih <melih.firat@ogr.sakarya.edu.tr>
2021-03-26 12:04:35 +0300

    Beautify code

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,37 +1,50 @@
 'use strict';
 
 // INITIAL
-document.querySelector('.close-about').style.display='none';
+var [i, j, k] = [0, 0, 0];
+document.querySelector('.close-about').style.display = 'none';
+let txtArray = ['a Developer', 'a Programmer', 'a Student', 'Melih']
+var txt = txtArray[k];
+var speed = 150;
+var textToType = document.getElementById("typer");
+var cursor = document.getElementById("cursor");
+var stopped = true;
+var url = window.location.href;
+const info = document.querySelector('#info');
+const resume = document.querySelector('#resume');
+const portfolio = document.querySelector('#portfolio');
+const tools = document.querySelector('#tools');
+const about = document.querySelector('#about');
+const sideNav = document.querySelector('.side-nav');
+const header = document.querySelector('header');
 
-window.addEventListener("scroll", ()=>
-{
-    if(scrollX<1366)
-    {
+let navIsClicked = false;
 
+url = url.substring(0, url.lastIndexOf("#"));
+window.history.replaceState(null, null, url);
+
+
+Typer();
+
+// FUNCTIONS
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function bookEffect() {
+    var y = 35;
+    $('.left').css("display", `block`);
+    $('.right').css("display", `block`);
+    for (let x = 0; x < 35; x++) {
+        y--;
+        await sleep(10);
+        $('.left').css("transform", `skewY(${y}deg)`);
+        $('.left').css("-ms-transform", `skewY(${y}deg)`);
+        $('.right').css("transform", `skewY(${-y}deg)`);
+        $('.right').css("-ms-transform", `skewY(${-y}deg)`);
     }
-})
-
-
-async function smth()
-{
-var y=35;
-$('.left').css("display",`block`); 
-$('.right').css("display",`block`); 
-for(let x=0; x<35; x++)
-{
- 
-    y--;
-    await sleep(10);
-    // Standard syntax
-    $('.left').css("transform",`skewY(${y}deg)`); 
-    $('.left').css("-ms-transform",`skewY(${y}deg)`); 
-    $('.right').css("transform",`skewY(${-y}deg)`); 
-    $('.right').css("-ms-transform",`skewY(${-y}deg)`); 
-}
 }
 
-
-//Typer Coded by github.com/firatmelih && Decleration phase.
 async function Typer() {
     if (j == txt.length) {
         setTimeout(cursorBlink, speed);
@@ -42,8 +55,7 @@ async function Typer() {
         i++;
         setTimeout(Typer, speed);
         j = i;
-    }
-    else if (j > 0) {
+    } else if (j > 0) {
         textToType.innerHTML = textToType.innerHTML.slice(0, -1);
         j--;
         setTimeout(Typer, (speed / 3));
@@ -58,6 +70,7 @@ async function Typer() {
         }
     }
 }
+
 async function cursorBlink() {
     for (let counter = 0; counter < 3; counter++) {
         cursor.innerHTML += '|';
@@ -67,106 +80,72 @@ async function cursorBlink() {
     }
     cursor.innerHTML = '';
 }
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-// Calling and running functions
-var [i, j, k] = [0, 0, 0];
-let txtArray = ['a Developer', 'a Programmer', 'a Student', 'Melih']
-var txt = txtArray[k];
-var speed = 150;
-var textToType = document.getElementById("typer");
-var cursor = document.getElementById("cursor");
-Typer();
 
-var url = window.location.href;
-url = url.substring(0, url.lastIndexOf("#")); 3
-window.history.replaceState(null, null, url);
+// ON SCROLL FUNCTIONS
 
-let navIsClicked = false;
+window.addEventListener("scroll", () => {
+    if (scrollX < 1366) {
+
+    }
+})
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 30 && !navIsClicked) {
+        sideNav.style.display = 'flex'
+        header.classList.add('scrolled');
+    }
+    if (navIsClicked && window.scrollY > 30) {
+        header.classList.add('scrolled');
+        sideNav.style.display = 'flex';
+    } else if (window.scrollY == 0) {
+        sideNav.style.display = 'none';
+        header.classList.remove('scrolled');
+        if (window.screen.width < 1366) {
+            header.classList.add('scrolled');
+        }
+    }
+    if (window.screen.width > 1366 && window.scrollY < 30 && navIsClicked) {
+        header.classList.remove('scrolled');
+        header.classList.remove('clicked');
+    }
+
+    if (window.screen.width > 1366 && window.scrollY > 30 && navIsClicked) {
+        header.classList.add('scrolled');
+        header.classList.add('clicked');
+    }
+
+})
+
+
+// ONCLICK FUNCTIONS
 
 document.querySelector('.side-nav').addEventListener('click', () => {
     navIsClicked = !navIsClicked;
     if (navIsClicked) {
         document.querySelector('.side-nav-img').classList.add('close');
         document.querySelector('.close').classList.remove('side-nav-img');
-        // document.querySelector('#welcome').classList.add('scrolled');
-        document.querySelector('header').classList.add('scrolled');
-        document.querySelector('header').classList.add('clicked');
-    }
-    else {
+        header.classList.add('scrolled');
+        header.classList.add('clicked');
+    } else {
         document.querySelector('.close').classList.add('side-nav-img');
         document.querySelector('.side-nav-img').classList.remove('close');
-        document.querySelector('.side-nav').style.display = 'flex';
-        document.querySelector('header').classList.remove('clicked');
+        sideNav.style.display = 'flex';
+        header.classList.remove('clicked');
     }
 })
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 30 && !navIsClicked) {
-        // document.querySelector('header').style.display = 'none';
-        document.querySelector('.side-nav').style.display = 'flex'
-        // document.querySelector('#welcome').classList.add('scrolled');
-        document.querySelector('header').classList.add('scrolled');
-    }
-    if (navIsClicked && window.scrollY > 30) {
-        document.querySelector('header').classList.add('scrolled');
-        // document.querySelector('#welcome').classList.add('scrolled');
-        document.querySelector('.side-nav').style.display = 'flex';
-    }
-    else if (window.scrollY == 0) {
-        // document.querySelector('header').style.display = 'block';
-        document.querySelector('.side-nav').style.display = 'none';
-        document.querySelector('header').classList.remove('scrolled');
-        // document.querySelector('#welcome').classList.remove('scrolled');
-        if(window.screen.width<1366)
-        {
-            document.querySelector('header').classList.add('scrolled');
-        }
-    }
-    if(window.screen.width>1366 && window.scrollY < 30 && navIsClicked) 
-    {
-        document.querySelector('header').classList.remove('scrolled');
-        // document.querySelector('#welcome').classList.remove('scrolled');
-        document.querySelector('header').classList.remove('clicked');
-    }
-    
-    if(window.screen.width>1366 && window.scrollY > 30 && navIsClicked)
-    {
-        document.querySelector('header').classList.add('scrolled');
-        // document.querySelector('#welcome').classList.add('scrolled');
-        document.querySelector('header').classList.add('clicked');
-    }
-    
-})
 
-
-
-
-const info = document.querySelector('#info');
-const resume = document.querySelector('#resume');
-const portfolio = document.querySelector('#portfolio');
-const tools = document.querySelector('#tools');
-
-
-
-
-const about = document.querySelector('#about');
-var stopped=true;
-
-about.addEventListener('click', ()=>
-{
+about.addEventListener('click', () => {
     document.querySelector('#hiddens').style.opacity = '1';
-    window.location.href='#about';
+    window.location.href = '#about';
     resume.classList.add('shift-right');
     tools.classList.add('shift-right');
     info.classList.add('shift-left');
     portfolio.classList.add('shift-left');
-    document.querySelector('.close-about').style.display='block';
+    document.querySelector('.close-about').style.display = 'block';
 })
 
-document.querySelector('.close-about').addEventListener('click', ()=>
-{
-    document.querySelector('.close-about').style.display='none';
+document.querySelector('.close-about').addEventListener('click', () => {
+    document.querySelector('.close-about').style.display = 'none';
     document.querySelector('#hiddens').style.opacity = '0';
 
     document.querySelector('body').classList.add('stop-scrolling');
@@ -175,65 +154,51 @@ document.querySelector('.close-about').addEventListener('click', ()=>
     info.classList.add('shift-left');
     portfolio.classList.add('shift-left');
 
-        resume.classList.remove('shift-right');
-        tools.classList.remove('shift-right');
-        info.classList.remove('shift-left');
-        portfolio.classList.remove('shift-left');
-    
-    if(!stopped)
-    {
+    resume.classList.remove('shift-right');
+    tools.classList.remove('shift-right');
+    info.classList.remove('shift-left');
+    portfolio.classList.remove('shift-left');
+
+    if (!stopped) {
         document.querySelector('body').classList.add('stop-scrolling');
     }
-    stopped=!stopped;
+    stopped = !stopped;
 })
 
-document.querySelector('.close-about').addEventListener('click',()=>
-{
-    
+document.querySelector('.close-about').addEventListener('click', () => {
     document.querySelector('body').classList.remove('stop-scrolling');
-
-   
 })
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-  
-
-
-resume.addEventListener('click',()=>
-{
-    smth();
-    document.querySelector('.close-about').style.display='block';
+resume.addEventListener('click', () => {
+    bookEffect();
+    document.querySelector('.close-about').style.display = 'block';
     $(".hidden-resume").css("display", "flex");
     $(".hidden-tools").css("display", "none");
     $(".hidden-portfolio").css("display", "none");
     $(".hidden-about").css("display", "none");
 
-    
+
 })
-tools.addEventListener('click',()=>
-{
-    document.querySelector('.close-about').style.display='block';
+tools.addEventListener('click', () => {
+    document.querySelector('.close-about').style.display = 'block';
     $(".hidden-resume").css("display", "none");
     $(".hidden-tools").css("display", "block");
     $(".hidden-portfolio").css("display", "none");
     $(".hidden-about").css("display", "none");
 })
-info.addEventListener('click',()=>
-{
-    document.querySelector('.close-about').style.display='block';
+
+info.addEventListener('click', () => {
+    document.querySelector('.close-about').style.display = 'block';
     $(".hidden-resume").css("display", "none");
     $(".hidden-tools").css("display", "none");
     $(".hidden-portfolio").css("display", "none");
     $(".hidden-about").css("display", "block");
 })
-portfolio.addEventListener('click',()=>
-{
-    document.querySelector('.close-about').style.display='block';
+
+portfolio.addEventListener('click', () => {
+    document.querySelector('.close-about').style.display = 'block';
     $(".hidden-resume").css("display", "none");
     $(".hidden-tools").css("display", "none");
     $(".hidden-portfolio").css("display", "block");
     $(".hidden-about").css("display", "none");
 })
-
